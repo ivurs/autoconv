3e6cac83666318addfc115a1c455bb46406796c2
mz <zhangqzmichael@gmail.com>
2025-09-28 18:00:50 +1000

    fab

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric import Connection, task
+
+REMOTE_HOST = "209.38.25.194"
+REMOTE_USER = "root"
+REMOTE_PASSWORD = "Ivurs+9909Zqz"
+REMOTE_DIR = "/home/root/autoconv"
+
+@task
+def deploy(c):
+    c = Connection(
+        host=REMOTE_HOST,
+        user=REMOTE_USER,
+        connect_kwargs={"password": REMOTE_PASSWORD}
+    )
+    c.run(f"cd {REMOTE_DIR} && git pull")
+    c.run(f"cd {REMOTE_DIR}/frontend && npm install")
+    c.run(f"cd {REMOTE_DIR}/frontend && npm run serve")
+    c.run(f"cd {REMOTE_DIR}/backend && pip install -r requirements.txt")
+    c.run("sudo systemctl restart autoconv")
