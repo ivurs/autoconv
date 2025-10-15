f4d5026cdb89d3635ba9caf3ef1909eeb49c4927
unknown <informationget@qq.com>
2025-10-15 23:07:01 +0800

    1.增加文件执行路径（windows上找不到APP路径）

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
 
 from fastapi import FastAPI
 #from starlette.middleware.cors import CORSMiddleware
